d662050371ace15f45d59b14e86f9f8215a4048c
Abilash Suresh <abilash.suresh199@gmail.com>
2025-03-01 22:41:21 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     while True:
         conn, addr = server_socket.accept() # wait for client
         r_msg = conn.recv(1024).decode('utf-8')
-        # r_msg = "GET /echo/banana HTTP/1.1\r\nHost: localhost:4221\r\n\r\n"
+        # r_msg = "GET /user-agent HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: pineapple/raspberry\r\n\r\n"
         parsed_msg = parse(r_msg)
 
         s_msg = create_msg(parsed_msg)
@@ -24,7 +24,7 @@ def deparse(msg):
     decoded = ""
     decoded += " ".join(msg["status_line"].values()) + "\r\n"
     if msg["headers"]:
-        decoded += "\r\n".join(msg["headers"])
+        decoded += "\r\n".join(f"{key}: {value}" for key,value in msg["headers"].items())
     decoded += "\r\n\r\n" 
     if msg["response_body"]:
         decoded += msg["response_body"]
@@ -40,23 +40,25 @@ def create_msg(msg):
 
     http_response = {
         "status_line": status_line,
-        "headers": [],
+        "headers": {},
         "response_body": "",
     }
-    #split by / take first arg, the first arg is "" since it's /foo/bar = ["",foo,bar]
-    #target = msg["request_line"]["request_target"].split("/")
-    target = msg["request_line"]["request_target"]
 
     # host_args = msg["host"].split(":")
     # assert (host_args[0] == "Host")
     
+
+
+    #split by / take first arg, the first arg is "" since it's /foo/bar = ["",foo,bar]
+    #target = msg["request_line"]["request_target"].split("/")
+    target = msg["request_line"]["request_target"]
+
     # target is right after GET 
     if target == "/":
         http_response["status_line"]["return_code"] = "200"
         http_response["status_line"]["status"] = "OK"
 
 
-        return http_response
     elif target.startswith("/echo"):
 
         http_response["status_line"]["return_code"] = "200"
@@ -65,25 +67,33 @@ def create_msg(msg):
         # removing the /echo/
         content = target[6:]
 
-        content_type = "Content-Type: text/plain"
-        content_length = f"Content-Length: {len(content)}"
-
-        http_response["headers"].append(content_type)
-        http_response["headers"].append(content_length)
+        http_response["headers"]["Content-Type"] = "text/plain"
+        http_response["headers"]["Content-Length"] = str(len(content))
 
         http_response["response_body"] = content
 
-        return http_response
     
+    elif target == "/user-agent":
+        http_response["status_line"]["return_code"] = "200"
+        http_response["status_line"]["status"] = "OK"
+
+        content = msg["headers"]["User-Agent"]
+        
+        http_response["headers"]["Content-Type"] = "text/plain"
+        http_response["headers"]["Content-Length"] = str(len(content))
+
+        http_response["response_body"] = content
+
     else:
         http_response["status_line"]["return_code"] = "404"
         http_response["status_line"]["status"] = "Not Found"
 
-        return http_response
+    return http_response
 
 def parse(msg):
     args = msg.split("\r\n")
     args[0] = args[0].split(" ")
+    args = list(filter(None, args))
 
 
     # "\n\r".join(http_request)
@@ -95,11 +105,13 @@ def parse(msg):
 
     http_request = {
         "request_line": request_line,
-        "headers":[],
+        "headers":{},
     }
 
     for arg in args[1:]:
-        http_request["headers"].append(arg)
+        # using a dictionary since it seems more resonable to
+        pair = arg.split(":",1)
+        http_request["headers"][pair[0]] = pair[1].strip()
     #print(http_request)
     return http_request
 
